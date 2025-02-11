9846a0b25ec6f3a74e20e5b804aa7ffc5637bd38
jthamayo <jtamayobalogh@gmail.com>
2025-02-12 00:41:13 +0100

    feat: add hero in and out animations on scroll event

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -2,52 +2,48 @@ import ContactInfo from "../contactInfo/ContactInfo";
 import { useState, useEffect } from "react";
 
 const Hero = () => {
-  const [isFixed, setIsFixed] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
   useEffect(() => {
     const handleScroll = () => {
       const heroHeight = document.getElementById("hero")?.offsetHeight || 0;
-      setIsFixed(window.scrollY > heroHeight * 2);
+      if (window.scrollY >= heroHeight) {
+        setIsScrolled(true);
+      }
+      if (window.scrollY <= heroHeight * 0.3) {
+        setIsScrolled(false);
+      }
     };
 
     window.addEventListener("scroll", handleScroll);
-    /* return () => window.removeEventListener("scroll", handleScroll); */
-  }, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
 
+  console.log(isScrolled);
   return (
-    <div className="h-screen w-screen flex justify-end items-end">
+    <div
+      id="home"
+      className="h-screen w-screen flex justify-end items-end shadow-lg"
+    >
       <div
         id="hero"
-        className={`transition-all ease-in-out duration-50 ${
-          isFixed
-            ? "fixed items-center top-0 left-0 z-40 w-1/4 h-screen p-4 shadow-lg flex flex-col bg-white"
-            : "relative w-4/5 inline-grid grid-rows-2 grid-cols-3"
-        }`}
+        className="transition-all ease-in-out duration-50 relative w-4/5 inline-grid grid-rows-2 grid-cols-3"
       >
         <div
-          className={`${
-            isFixed
-              ? "text-start text-7xl w-full"
-              : "col-span-2 row-start-2 flex items-start justify-end text-nowrap text-8xl text-end leading-25 px-4"
-          }`}
+          className={`col-span-2 row-start-2 flex items-start justify-end text-nowrap text-8xl text-end leading-25 px-4 transition-all ease-in-out duration-600 ${isScrolled? "-translate-x-400 opacity-0": "translate-x-0 opacity-100"}`}
         >
           <h1 className="font-light uppercase">Judith Tamayo</h1>
         </div>
         <div
-          className={`flex items-end ${
-            isFixed
-              ? "text-start text-5xl w-full"
-              : "text-end col-start-2 row-start-1 text-6xl px-4"
-          }`}
+          className={`flex items-end text-end col-start-2 row-start-1 text-6xl px-4 tracking-wider justify-end transition-all ease-in-out duration-400 ${isScrolled? "-translate-y-400 opacity-0": "translate-y-0 opacity-100"}`}
         >
-          <h2 className="font-extralight tracking-wider uppercase">
-            Full Stack Developer
+          <h2 className="font-extralight uppercase">
+            Full<span className="block">Stack</span>Developer
           </h2>
         </div>
-        <div
-          className={`flex items-end ${
-            isFixed ? "w-full" : "col-start-3 row-start-1 px-4"
-          }`}
-        >
+        <div className={`flex items-end col-start-3 row-start-1 px-4 transition-all ease-in-out duration-800 ${isScrolled? "translate-x-400 opacity-0": "translate-x-0 opacity-100"}`}>
           <ContactInfo />
         </div>
       </div>
