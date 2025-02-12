9cdee52cc6c29a3dc50c5de2e10372e3e16393cb
jthamayo <jtamayobalogh@gmail.com>
2025-02-12 20:53:49 +0100

    feat: lift Navbar isOpen state to Main to change web's main content margin conditionally

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -2,20 +2,27 @@ import Navbar from "../navbar/Navbar";
 import TechStack from "../techStack/TechStack";
 import Toolbox from "../toolbox/Toolbox";
 import ProjectShowcase from "../projectShowcase/ProjectShowcase";
+import { useState } from "react";
 
 const Main = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="w-screen h-full flex justify-stretch items-stretch @container/nav">
-      <div className="h-[3000px] w-[40px] @lg/nav:w-[400px]">
-        <Navbar />
-      </div>
-      <div className="h-full w-full">
-        <main className="w-full sm:px-5 md:px-10 lg:px-20 xl:px-30">
-          <TechStack />
-          <Toolbox />
-          <ProjectShowcase />
-        </main>
-      </div>
+    <div className="w-full h-full flex justify-stretch items-stretch">
+      <Navbar isOpen={isOpen} handleOpen={handleOpen} />
+      <main
+        className={`w-full sm:px-5 md:px-10 lg:px-15 xl:px-40 transition-all delay-100 duration-500 ${
+          isOpen ? "ml-[300px] " : "ml-[50px] xl:px-60"
+        }`}
+      >
+        <TechStack />
+        <Toolbox />
+        <ProjectShowcase />
+      </main>
     </div>
   );
 };
